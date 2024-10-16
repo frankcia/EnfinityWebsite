ce31a1f931e4c2371be727c0ff55bab7d72ea311
Frankie Garcia <frankcia@amazon.com>
2024-10-16 19:24:55 -0400

    Initial Setup

---
+++ src/components/EnfinityWordLogo.tsx
@@ -0,0 +1,19 @@
+export default function EnfinityWordLogo() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 170 20" width={170}>
+            <g>
+                <g id="Layer_2" display="none">
+                    <rect width="170" height="20" fill="#231f20"/>
+                </g>
+                <g id="Layer_1">
+                    <text transform="translate(4.6 16.9)" font-size="20">
+                        <tspan x="0" y="0" fill="#f2f2f2" font-family="EthnocentricEl-Regular, Ethnocentric">Enfinity
+                        </tspan>
+                        <tspan x="104.9" y="0" fill="#ed1c24" font-family="EthnocentricLt-Regular, Ethnocentric">USA
+                        </tspan>
+                    </text>
+                </g>
+            </g>
+        </svg>
+    );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import EnfinityWordLogo from "@/components/EnfinityWordLogo";
+
+export default function Header() {
+  return (
+    <div>
+        <EnfinityWordLogo />
+    </div>
+  );
+}
